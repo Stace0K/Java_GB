70f5bce4e8a1ead87d73b4bcf2399300fa6cba40
anstice <ko.an.2018@yandex.ru>
2023-06-29 18:22:42 +0300

    task2 is done

--- Homework/HW1/Program.java
+++ Homework/HW1/Program.java
@@ -1,13 +1,16 @@
 package Homework.HW1;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
         boolean task = true;
-        while(task){
+        while (task) {
             System.out.println("Какую задачу проверяем? Ответ цифрой:");
             System.out.println("1 - Первую");
             System.out.println("2 - Вторую");
@@ -23,15 +26,15 @@ public class Program {
                     task1Factorial(n);
                     break;
                 case 2:
-                    
+                    task2();
                     break;
                 case 3:
-                    
+
                     break;
                 case 0:
-                    
+                    System.out.println("^_^");
+                    task = false;
                     break;
-            
                 default:
                     System.out.println("У нас тут таких значений нет, давайте по новой.");
                     break;
@@ -39,7 +42,7 @@ public class Program {
         }
     }
 
-    static void task1Triangle(int n){
+    static void task1Triangle(int n) {
         int sum = 0;
         for (int i = 0; i <= n; i++) {
             sum += i;
@@ -47,11 +50,36 @@ public class Program {
         System.out.printf("Треугольное число числа %d: %d%n", n, sum);
     }
 
-    static void task1Factorial(int n){
+    static void task1Factorial(int n) {
         int multi = 1;
         for (int i = 0; i < n; i++) {
             multi *= i + 1;
         }
         System.out.printf("Факториал числа %d: %d%n", n, multi);
+    }
+
+    private static boolean isPrime(int nu) {
+        if (nu <= 1)
+            return false;
+        for (int i = 2; i * i <= nu; i++) {
+            if (nu % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void task2() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i <= 1000; i++) {
+            if (isPrime(i)) {
+                numbers.add(i);
+            }
+        }
+        System.out.println("Простые числа в интервале [1, 1000]: " + numbers);
+    }
+
+    static void task3(){
+        
     }
 }
